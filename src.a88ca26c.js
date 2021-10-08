530670a602dc1d2fb68b71130ec402c9e7bcfaf7
vitaliy-1111 <networkvetalius@gmail.com>
2021-10-08 10:34:21 +0000

    Deploying to gh-pages from @ vitaliy-1111/team-project-icecream@680efac946f43f413d54ad40f00462eb1cf42e9f 🚀

--- src.a88ca26c.js
+++ src.a88ca26c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-icecream/src.df9d0e10.js.map
+//# sourceMappingURL=/team-project-icecream/src.a88ca26c.js.map
